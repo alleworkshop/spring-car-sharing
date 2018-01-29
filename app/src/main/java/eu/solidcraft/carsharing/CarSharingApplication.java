a3ea09ac6804aaa4be37e2857912de47c06e54de
Jakub Nabrdalik <jakubn@gmail.com>
2018-01-29 09:31:06 +0100

    brand new project from spring.io

---
+++ app/src/main/java/eu/solidcraft/carsharing/CarSharingApplication.java
@@ -0,0 +1,12 @@
+package eu.solidcraft.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSharingApplication.class, args);
+	}
+}
